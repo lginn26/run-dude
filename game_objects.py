1085f1d79fd0fe6f6a0c8a8a660a6181536e0af9
Louis Godfrey <lginn26@users.noreply.github.com>
2020-05-06 18:41:51 -0400

    Updated Player class   - Created graphics related variables   - Created Player.change_sprite()   - Updated Player.update()   - Updated Player.update() description

--- game_objects.py
+++ game_objects.py
@@ -5,17 +5,40 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
 
+        # graphical variables
+        self.max_frame_dur = 4
+        self.cur_frame_dur = 4
+        self.cur_frame = 0
+
+        self.walk_cycle = [
+            pygame.image.load('assets/sprites/rundude-walk00.png'),
+            pygame.image.load('assets/sprites/rundude-walk01.png'),
+            pygame.image.load('assets/sprites/rundude-walk02.png'),
+            pygame.image.load('assets/sprites/rundude-walk01.png')
+        ]
+
+        # basic variables
         self.image = pygame.image.load('assets/sprites/rundude-walk00.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
-        # graphical variables
-        self.cur_frame_dur = 10
-
         # logic variables
         self.speed = 10
         self.jump_dur = 10
+
+    def change_sprite(self):
+        if self.cur_frame_dur > 0:
+            self.cur_frame_dur -= 1
+        else:
+            print("frame shift")
+            self.cur_frame += 1
+            self.cur_frame_dur = self.max_frame_dur
+
+            if self.cur_frame > len(self.walk_cycle) - 1:
+                self.cur_frame = 0
+
+        self.image = self.walk_cycle[self.cur_frame]
 
     def controls(self, control):
         """
@@ -31,6 +54,11 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.speed
 
     def update(self, us_input):
+        """
+        Makes instances preform intended actions
+        :param us_input: Short for user input, intakes the player's keyboard presses
+        """
         self.controls(us_input)
+        self.change_sprite()
 
 
